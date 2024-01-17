ee0f45cff018509b3094a57ad04a99f085742f34
SamsonVH <samson.vh.20@gmail.com>
2024-01-17 13:13:25 +0700

    Test domain

--- testapp/app/test/page.tsx
+++ testapp/app/test/page.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 const page = async () => {
-  const a = await fetch("https://api.publicapis.org/entries").then(res => res.json());
+  const a = await fetch("https://capstonefa23.vn/api/v1/membership-packages").then(res => res.json());
 
-  return <div>{a.count}</div>;
+  return <div>{a.pageCount}</div>;
 };
 
 export default page;
